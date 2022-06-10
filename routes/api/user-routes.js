9dfdb29dffe8b33e1d8408dcd37f33fbdfae21a1
Nicole Barkley <barkleylikecharles@icloud.com>
2022-06-09 20:55:32 -0400

    user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+  } = require('../../controllers/users-controller');
+// Set up GET all and POST at /api/pizzas
+router
+  .route('/')
+  .get(getAllUsers)
+  .post(createUser);
+
+// Set up GET one, PUT, and DELETE at /api/pizzas/:id
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+  // Set up POST and DELETE for Friends
+route.route('/:id/friends/:friendsId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
